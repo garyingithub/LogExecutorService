2db21c8fb9a1e134d8c4ae23f1e0967511c81a95
garyingithub <laiwei110>
2017-09-05 09:52:08 +0800

    commit

--- src/main/java/org/yawlfoundation/cloud/LogExectorServiceApplication.java
+++ src/main/java/org/yawlfoundation/cloud/LogExectorServiceApplication.java
@@ -1,15 +1,21 @@
 package org.yawlfoundation.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceB_EngineBasedClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceB_EnvironmentBasedServer;
-
+import org.yawlfoundation.cloud.metrics.InfluxDBGaugeWriter;
 /**
  * Created by gary on 16/04/2017.
  */
+
 @SpringBootApplication
 public class LogExectorServiceApplication {
 
@@ -17,9 +23,15 @@ public class LogExectorServiceApplication {
         SpringApplication.run(LogExectorServiceApplication.class, args);
     }
 
+
+
+
+
     @Bean
-    ServletRegistrationBean testServletRegistrationBean(){
-        return new ServletRegistrationBean(new InterfaceB_EnvironmentBasedServer(),"/test/*");
+    ServletRegistrationBean testServletRegistrationBean(InfluxDBGaugeWriter writer,XMLHelper helper){
+        InterfaceB_EnvironmentBasedServer server=new InterfaceB_EnvironmentBasedServer();
+        server.set_controller(new LogExectorController(writer,helper));
+        return new ServletRegistrationBean(server,"/test/*");
     }
 
 }
--- src/main/java/org/yawlfoundation/cloud/ProcessInstance.java
+++ src/main/java/org/yawlfoundation/cloud/ProcessInstance.java
@@ -1,29 +1,88 @@
 package org.yawlfoundation.cloud;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * Created by gary on 16/04/2017.
  */
 public class ProcessInstance {
 
+
+
     private String id;
-    private Map<String,Long> taskDurationTimeMap;
 
+    private List<Pair<String,Long>> taskDurationList=new ArrayList<>();
+    private List<Pair<String,Long>> backup=new ArrayList<>();
 
+    final private Logger logger= LoggerFactory.getLogger(ProcessInstance.class);
+
+    public void storeBackup(){
+
+        for(Pair<String,Long> pair:taskDurationList){
+            backup.add(pair);
+        }
+
+    }
+
+    public void restore(){
+
+        taskDurationList.clear();
+        for(Pair<String,Long> pair:backup){
+            taskDurationList.add(pair);
+        }
+
+    }
     public ProcessInstance(String id){
         this.id=id;
-        this.taskDurationTimeMap=new HashMap<String, Long>();
+        this.taskDurationList=new ArrayList<>();
     }
 
     public void addTask(String taskName,Long durationTime){
-        this.taskDurationTimeMap.put(taskName,durationTime);
+        this.taskDurationList.add(new Pair<>(taskName, durationTime/100 ));
+
     }
 
     // 单位为毫秒
     public Long getDurationTime(String taskName){
-        return taskDurationTimeMap.get(taskName);
+        Pair<String ,Long> temp=null;
+        for(Pair<String,Long> pair:this.taskDurationList){
+            if(pair.getKey().equals(taskName)){
+                temp=pair;
+                break;
+            }
+        }
+        Long result;
+        if(temp==null){
+            result=100L;
+            logger.info("Wrong log "+taskName);
+            restore();
+        }else {
+            result = temp.getValue();
+            this.taskDurationList.remove(temp);
+            if(this.taskDurationList.size()==0){
+                this.restore();
+            }
+        }
+
+        return result;
+    }
+
+
+    public Pair<String, Long> next(String[] taskNames){
+
+        for(Pair<String,Long> pair:this.taskDurationList){
+            for(String taskName:taskNames) {
+                if (taskName.equals(pair.getKey())) {
+                    return pair;
+                }
+            }
+        }
+        this.restore();
+        throw new RuntimeException("No such task");
     }
 
 
